2d1a2d4d69da318bdb420f47b7f5e8cc74e349bb
Daniel0226 <batehampandora@gmail.com>
2019-04-20 19:36:26 -0400

    added additional files

---
+++ Login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Signup</title>
+        <meta charset="utf-8" />
+        <link href="signup.gif" type="image/gif" rel="shortcut icon" />
+        <script src="http://ajax.googleapis.com/ajax/libs/prototype/1.7.0.0/prototype.js" type="text/javascript"></script>
+
+    </head>
+    <body>
+        <?php include("top.html"); ?>
+        <div>
+            <form action="login-submit.php" method="post" enctype="multipart/form-data">
+                <fieldset>
+                    <legend><strong>Log in</strong></legend>
+                </fieldset>
+            </form>
+        </div>
+    </head>
+</html>
