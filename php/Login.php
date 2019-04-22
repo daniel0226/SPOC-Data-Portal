b93b353517945d1bd83259e3f28996aa4664a7fe
Daniel0226 <batehampandora@gmail.com>
2019-04-22 13:34:14 -0400

    Finished first version of Login page

--- php/Login.php
+++ php/Login.php
@@ -1,16 +1,31 @@
 
 <?php include("../html/top.html"); ?>
-<div id="login">
-    <form action="login-submit.php" method="get" enctype="multipart/form-data">
-        <div class = "container">
-            <h1><strong>Log in</strong></h1>
-            <input type="text" placeholder="User Name" name="Username" required>
-            <input type="password" placeholder="Password" name="Password" required id="PasswordField">
-            <input type="submit" value="Log in">
-            <a href="../php/RecoverPassword.php">Recover Password</a>
-            <input type="checkbox" onclick="showPassword()"> Show Password
-            <a href="../php/RequestAccess.php">Request Access</a>
-            <p>If you want access to raw data you can request access.</p>
+<div>
+    <form action="login-submit.php" method="get" enctype="multipart/form-data" class="login">
+        <div class = "container containerLogin">
+            <div class="row">
+                <h1><strong>Log in</strong></h1>
+            </div>
+            <div class ="row">
+                <input type="text" placeholder="User Name" name="Username" required>
+            </div>
+            <div class ="row">
+                <input type="password" placeholder="Password" name="Password" required id="PasswordField">
+            </div>
+            <div class ="row">
+                <input type="submit" value="Log in">
+            </div>
+            <div class ="row">
+                <a href="../php/RecoverPassword.php">Recover Password</a>
+                <input type="checkbox" onclick="showPassword()">
+                <p class="floatRight">Show Password</p>
+            </div>
+            <div class ="row">
+                <a href="../php/RequestAccess.php">Request Access</a>
+            </div>
+            <div class ="row">
+                <p>If you want access to raw data you can request access.</p>
+            </div>
         </div><!-- container container-table -->
     </form> <!-- form login-submit.php-->
 </div><!-- Login -->
