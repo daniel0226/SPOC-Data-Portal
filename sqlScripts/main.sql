0dff89d05aac6d1ea19f23364a1b9cc372cf1bdf
James Roach <jhroach14@gmail.com>
2019-04-23 16:39:14 -0400

    added data base. now its time for the php connections

---
+++ sqlScripts/main.sql
@@ -0,0 +1,44 @@
+/*Author: James Roach.
+This is the script used to build the main database for the site
+ */
+
+/*Table for user managment*/
+create table users(
+	id int auto_increment not null,
+	username varchar(256),
+	password varchar(256),
+	enabled boolean,
+	email varchar(256),
+
+	primary key(id)
+);
+
+/*Table for photo info. Actual photo data is stored in 
+file system not in data base*/
+create table photos(
+	id int auto_increment not null,
+	photo_path varchar(1028) not null,
+	photo_text varchar(256),  /*mouse over text*/
+
+	primary key(id)
+);
+
+/*The holds the info for posts on the main page*/
+create table blog_posts(
+	id int auto_increment not null,
+	post_title varchar(128) not null,
+	post_date datetime not null,
+	post_text varchar(16384),
+	photo int,
+
+	primary key(id),
+
+	foreign key (photo) references photos(id)
+	on delete set null
+);
+
+create table archive_links(
+	id int auto_increment not null,
+	link_text varchar(256) not null,
+	link_link varchar(1028) not null
+);
