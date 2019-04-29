a1bab08f529982267e499cf723bfce097258ff1e
Daniel0226 <batehampandora@gmail.com>
2019-04-29 19:40:30 -0400

    Added database php connect

--- php/Index.php
+++ php/Index.php
@@ -1,4 +1,22 @@
 
 <?php include("../html/top.html"); ?>
+
+<?php
+    //Connect to Database
+    //YOU'LL HAVE TO CHANGE THESE VALUES
+    $DB_SERVER = 'localhost:3306';
+    $DB_USERNAME = 'Test';
+    $DB_PASSWORD = '200149907Dk!';
+    $DB_DATABASE = 'termproject';
+    $db = mysqli_connect($DB_SERVER,$DB_USERNAME,$DB_PASSWORD,$DB_DATABASE);
+
+    session_start();
+
+    $UsersDB = "SELECT * FROM users";
+    $PhotosDB = "SELECT * FROM photos";
+    $BlogPostsDB = "SELECT * FROM blog_posts";
+    $ArchiveDB = "SELECT * FROM archive_links";
+?>
+
 <?php include("../html/body.html"); ?>
 <?php include("../html/bottom.html"); ?>
